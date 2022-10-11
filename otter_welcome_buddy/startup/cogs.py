fcb1d6190b3e0ca1b85b7477409e36386bfe4063
Roberto <reyesfragosoroberto@gmail.com>
2022-10-10 21:25:21 -0700

    Fix problem with relative path (#11)

--- otter_welcome_buddy/startup/cogs.py
+++ otter_welcome_buddy/startup/cogs.py
@@ -6,7 +6,7 @@ from discord.ext.commands import Bot
 def __format_module_path_into_cog_extension(absolute_module_path: str) -> str:
     """Transforms absolute module path into <base_path>.<cog_path>.<file>"""
     module_absolute_path_no_extension: str = absolute_module_path[:-3]
-    module_relative_path: list[str] = module_absolute_path_no_extension.split("/")[-3:]
+    module_relative_path: list[str] = module_absolute_path_no_extension.split("/")[-2:]
     return ".".join(module_relative_path)
 
 
@@ -17,7 +17,6 @@ async def register_cogs(bot: Bot) -> None:
     ]
 
     for cog in allowed_cogs:
-        pass
         # __file__ stores absolute path
-        # cog_extension = __format_module_path_into_cog_extension(cog.__file__)
-        # await bot.load_extension(cog_extension)
+        cog_extension = __format_module_path_into_cog_extension(cog.__file__)
+        await bot.load_extension(cog_extension)
