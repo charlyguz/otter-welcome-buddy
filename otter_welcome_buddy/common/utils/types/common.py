4e42ab82f8e7d7e1d943bcee57420262feaeb500
Manuel CH <manuel_7795@hotmail.com>
2023-01-12 18:53:27 -0600

    Update pre-commit and fix issues

--- otter_welcome_buddy/common/utils/types/common.py
+++ otter_welcome_buddy/common/utils/types/common.py
@@ -2,6 +2,4 @@ from typing import TypeAlias
 
 import discord
 
-DiscordChannelType: TypeAlias = (
-    discord.abc.GuildChannel | discord.Thread | discord.abc.PrivateChannel
-)
+DiscordChannelType: TypeAlias = discord.abc.GuildChannel | discord.Thread | discord.abc.PrivateChannel
